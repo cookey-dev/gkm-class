818f6a22e1e1bb244964771b1a498b56eafe9225
Rowan C <rowan.dev.email@gmail.com>
2023-04-06 18:44:03 -0700

    files didnt save so another commit

--- GKM.mjs
+++ GKM.mjs
@@ -3,22 +3,21 @@ import path from 'path';
 import { dirname } from 'desm';
 import { spawn } from 'child_process';
 
-class GKM {
-	events;
+class GKM extends EventEmitter2 {
 	gkm;
 	constructor() {
-		this.events = new EventEmitter2({wildcard: true});
+		super({wildcard: true});
 		this.gkm = spawn('java', ['-jar', path.join(dirname(import.meta.url), 'lib/gkm.jar')]);
 		this.gkm.stdout.on('data', (function(data) {
 			data = data.toString().split(/\r\n|\r|\n/).filter(function(item) { return item; });
 			for (var i in data) {
 				var parts = data[i].split(':');
-				this.events.emit(parts[0], parts.slice(1));
+				this.emit(parts[0], parts.slice(1));
 			}
 		}).bind(this));
 	}
 	quit() {
-		this.events.removeAllListeners('*');
+		this.removeAllListeners('*');
 		this.gkm.kill();
 	}
 }
